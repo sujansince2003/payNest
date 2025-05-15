cc843d916e4f2b5ee89f5c6d0c61510f1de8c1a2
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-15 14:50:11 +0545

    setup turborepo project: added tailwind css

---
+++ apps/c/app/page.tsx
@@ -0,0 +1,9 @@
+import Alert from "@repo/ui/alert";
+
+export default function Home() {
+  return (
+    <div>
+      <Alert />
+    </div>
+  );
+}
---
+++ packages/ui/src/alert.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const Alert = () => {
+  return <div className="bg-red-500 p-2">Alert hello</div>;
+};
+
+export default Alert;
