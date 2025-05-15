4ca3a8c3469ea119f85c1ef6c14799d81ce9c2ac
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-15 12:59:38 +0545

    session provider in the layout

--- apps/c/app/layout.tsx
+++ apps/c/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import SessionProviderWrapper from "./provider/SessionProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,9 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <SessionProviderWrapper>
+        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          {children}
+        </body>
+      </SessionProviderWrapper>
     </html>
   );
 }
