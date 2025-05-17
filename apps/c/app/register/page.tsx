5304ce454c0ce4502d9ffcd0fed4dfd9fb1aa6f0
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-17 15:32:46 +0545

    build register comp to handle user registration in user next app

--- apps/c/app/register/page.tsx
+++ apps/c/app/register/page.tsx
@@ -1,49 +1,123 @@
 "use client";
 
-import { useState } from "react";
-import React from "react";
+import Link from "next/link";
 
-const Page = () => {
+import React, { useState } from "react";
+
+const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
 
-  function handleRegister(e: React.FormEvent<HTMLFormElement>) {
+  async function handleRegister(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    setErrorMsg("");
     try {
-      fetch("/api/auth/register", {
+      const response = await fetch("/api/auth/register", {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify({ email, password }),
       });
+      const data = await response.json();
+
+      if (response.ok) {
+        window.location.href = "/login";
+      } else {
+        setErrorMsg(data.msg || "something went wrong");
+      }
+
+      console.log(response);
     } catch (error) {
       console.log(error);
     }
   }
-
   return (
-    <div>
-      <form onSubmit={handleRegister}>
-        <label htmlFor="username">username</label>
-        <input
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-          id="username"
-          type="text"
-        />
-        <label htmlFor="password">password</label>
-        <input
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          value={password}
-          id="password"
-          type="text"
-        />
-        <button>submit</button>
-      </form>
-    </div>
+    <section className="bg-gray-50">
+      <div className="flex flex-col items-center justify-center px-6  py-8 mx-auto md:h-screen lg:py-0">
+        <a
+          href="#"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 "
+        >
+          Register to PayNest.
+        </a>
+        <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0  ">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            {errorMsg && (
+              <div className="p-2 text-sm text-red-600 bg-red-100 rounded">
+                {errorMsg}
+              </div>
+            )}
+            <form
+              onSubmit={handleRegister}
+              className="space-y-4 md:space-y-6"
+              action="#"
+            >
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Email
+                </label>
+                <input
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5    "
+                  placeholder="Enter your Email"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Password
+                </label>
+                <input
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  value={password}
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5    "
+                  required
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="w-full  text-white bg-blue-500 
+                hover:bg-blue-600
+                focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+              >
+                Sign up
+              </button>
+              <p className="text-sm font-light text-gray-500 ">
+                Already have an account?{" "}
+                <Link
+                  href="/login"
+                  className="font-medium text-primary-600 hover:underline "
+                >
+                  Login
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
-export default Page;
+export default Register;
