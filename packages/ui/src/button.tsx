16fd5629eb90b658b90d32430d24ef5442c32ce0
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-19 13:00:41 +0545

    added disabled to btn

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -4,12 +4,14 @@ import { ReactNode } from "react";
 
 interface ButtonProps {
   children: ReactNode;
+  amount: number;
   onClick: () => void;
 }
 
-export const Button = ({ onClick, children }: ButtonProps) => {
+export const Button = ({ onClick, amount, children }: ButtonProps) => {
   return (
     <button
+      disabled={amount > 0 ? false : true}
       onClick={onClick}
       type="button"
       className="text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
