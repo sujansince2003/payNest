de3d16d558bfc78df4244a8382f3a15e3c11456c
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-19 21:12:39 +0545

    server action for handling P2P transactions

---
+++ apps/c/lib/actions/sendMoney.ts
@@ -0,0 +1,88 @@
+"use server";
+
+import prisma from "@repo/db/client";
+import { authOptions } from "../authOptions";
+import { getServerSession } from "next-auth";
+
+export async function handleP2P(amount: number, receiverEmail: string) {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    return {
+      msg: "unauthenticated request",
+    };
+  }
+  const receiverAccount = await prisma.user.findUnique({
+    where: {
+      email: receiverEmail,
+    },
+  });
+
+  if (!receiverAccount) {
+    return {
+      msg: "receiver doesnot exist",
+    };
+  }
+
+
+  if (Number(receiverAccount.id) === Number(session.user.id)) {
+    return {
+      msg: "you cannot send money to yourself",
+
+    };
+  }
+
+  try {
+    await prisma.$transaction(async (transactionClient) => {
+      const availabeAmt = await transactionClient.balance.findFirst({
+        where: {
+          userId: Number(session.user.id),
+        },
+      });
+      if (!availabeAmt) {
+        throw new Error("Sender has no balance record")
+      }
+
+
+
+      if (availabeAmt && availabeAmt?.amount < amount) {
+        throw new Error("Insufficient balance");
+
+      }
+
+      await transactionClient.balance.update({
+        where: {
+          userId: Number(session.user.id),
+        },
+        data: {
+          amount: {
+            decrement: amount,
+          },
+        },
+      });
+      await transactionClient.balance.update({
+        where: {
+          userId: receiverAccount.id,
+        },
+        data: {
+          amount: {
+            increment: amount,
+          },
+        },
+      });
+    });
+    return {
+      msg: `Successfully transfered to ${receiverAccount.email}  `
+    }
+  } catch (error) {
+    console.log(error);
+    if (error instanceof Error) {
+      return { msg: error && error.message || "Something went wrong" };
+    }
+    else {
+      return { msg: "Something went wrong" }
+    }
+
+
+  }
+}
