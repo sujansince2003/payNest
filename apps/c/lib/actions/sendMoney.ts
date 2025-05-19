8d4d5323bd561f38c6a87741b8324fa97784580f
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-19 23:13:01 +0545

    handle multiple request at same time by locking of rows in postgres db

--- apps/c/lib/actions/sendMoney.ts
+++ apps/c/lib/actions/sendMoney.ts
@@ -1,3 +1,4 @@
+// handles peer to peer transactions i.e transfer between the accounts of the application
 "use server";
 
 import prisma from "@repo/db/client";
@@ -24,16 +25,18 @@ export async function handleP2P(amount: number, receiverEmail: string) {
     };
   }
 
-
   if (Number(receiverAccount.id) === Number(session.user.id)) {
     return {
       msg: "you cannot send money to yourself",
 
     };
   }
-
   try {
     await prisma.$transaction(async (transactionClient) => {
+
+      // locking the balance so that the multiple requests are handled sequentially(one by one)
+      await transactionClient.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(session.user.id)} FOR UPDATE`;
+
       const availabeAmt = await transactionClient.balance.findFirst({
         where: {
           userId: Number(session.user.id),
@@ -43,13 +46,10 @@ export async function handleP2P(amount: number, receiverEmail: string) {
         throw new Error("Sender has no balance record")
       }
 
-
-
       if (availabeAmt && availabeAmt?.amount < amount) {
         throw new Error("Insufficient balance");
 
       }
-
       await transactionClient.balance.update({
         where: {
           userId: Number(session.user.id),
@@ -82,7 +82,5 @@ export async function handleP2P(amount: number, receiverEmail: string) {
     else {
       return { msg: "Something went wrong" }
     }
-
-
   }
 }
